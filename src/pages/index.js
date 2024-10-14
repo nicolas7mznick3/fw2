bd1550227d0b1436a9cef36f4e4d5d0cfac0d42c
Marcelo Tomporoski Perez <159451183+Marcelo-IFMS@users.noreply.github.com>
2024-10-14 14:21:29 +0000

    aula 14-10

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,12 @@
 import Footer from "./modulos/footer";
 import Menu from "./modulos/navbar";
-import {Container, Row,Col} from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 
 
 export default function Index() {
   return <div>
     <Menu />
     Bem Vindos a disciplina
-    
     <Footer />
   </div>;
 }
