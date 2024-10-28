7de7b6249d97d7adfe7456b00934f376f89bf15c
Marcelo Tomporoski Perez <159451183+Marcelo-IFMS@users.noreply.github.com>
2024-10-28 14:16:16 +0000

    Aula 28-10 - Promise, Await e Async

--- src/pages/promessa.js
+++ src/pages/promessa.js
@@ -1,18 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Footer from "./modulos/footer";
 import Menu from "./modulos/navbar";
 import { Container } from "react-bootstrap";
 export default function Promessa() {
-const [pao, setpao] = useState("")
-    setTimeout(() => {
-        document.title = "Promessa é dívida";
-        setpao("é dívida");
-    }, 4000);
+    const [pao, setpao] = useState("")
+    useEffect(() => {
+        setTimeout(async() => {
+            document.title = "Promessa é dívida";
+            const espera = await mudaPao("é uma dívida")
+            setpao(espera);
+        }, 4000)
+    });
     return <>
         <Menu />
         <Container>
-            Promessa {pao==="" ? <img src="./loading.gif" width={60} height={20}/> : pao}
+            Promessa {pao === "" ? <img src="./loading_2.gif" width={60} height={30} /> : pao}
         </Container>
         <Footer />
     </>
 }
+function mudaPao(texto) {
+   return new Promise((resolva) =>
+     setTimeout(() => {
+        resolva(texto)
+    }, 3000));
+}
+// var teste = "";
+// if ("1" == 1) { //true ==1 | true === 1 | false == 0 | false === 0
+//     teste = "veradeiro";
+// } else {
+//     teste = "falso";
+// }
